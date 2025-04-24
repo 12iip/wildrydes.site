285088fe31145084085347be71593b67a1ac9c10
Sudeep <105295956+12iip@users.noreply.github.com>
2025-04-25 00:15:08 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_6tBXwbcKD', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22dj5f8ncku5gd7tsd2ktc5ae3', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
